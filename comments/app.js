a8c9bf64da9ae82fd354ebaa07d6883a990141d7
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-10-13 20:10:27 +0530

    feat : sending event when new post and comment created

--- comments/app.js
+++ comments/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const { randomBytes, randomInt } = require('crypto');
 
@@ -16,13 +17,22 @@ app.get("/posts/:id/comments", (req, res) => {
     return res.status(200).send(comments);
 });
 
-app.post("/posts/:id/comments", (req, res)=>{
+app.post("/posts/:id/comments", async (req, res)=>{
     const commentId = randomBytes(4).toString('hex');
     const {content} = req.body;
     const comments = commentsByPostID[req.params.id] || [];
 
     comments.push({id: commentId, content});
     commentsByPostID[req.params.id] = comments;
+
+    await axios.post("http://localhost:4005/events", {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
 
     return res.status(201).send(comments);
 });
